b028edd187533da7cdbc3345beee07a0a24cbb82
Xtr126 <80520774+Xtr126@users.noreply.github.com>
2024-09-05 15:44:07 +0530

    refactor: move attributes to XML and dismiss on touch outside

--- app/src/main/java/com/ax86/startmenu/MainActivity.java
+++ app/src/main/java/com/ax86/startmenu/MainActivity.java
@@ -1,23 +1,14 @@
 package com.ax86.startmenu;
 
 import android.app.Activity;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.view.Gravity;
-import android.view.View;
-import android.view.WindowManager;
 
 import androidx.recyclerview.widget.RecyclerView;
 
-import java.util.function.Consumer;
-
 public class MainActivity extends Activity {
-
-    private final int mBackgroundBlurRadius = 20;
 
     // Use a rectangular shape drawable for the window background. The outline of this drawable
     // dictates the shape and rounded corners for the window background blur area.
-    private Drawable mWindowBackgroundDrawable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,39 +17,6 @@ public class MainActivity extends Activity {
         AppsGridAdapter adapter = new AppsGridAdapter(this);
         RecyclerView recyclerView = findViewById(R.id.apps_grid);
         recyclerView.setAdapter(adapter);
-
-        // Dismiss start menu when user clicks outside
-        View view = findViewById(R.id.root_view);
-        view.setOnClickListener(v -> this.finish());
-
-        mWindowBackgroundDrawable = getDrawable(R.drawable.window_background);
-        getWindow().setBackgroundDrawable(mWindowBackgroundDrawable);
-
-        setupWindowBlurListener();
-
-    }
-
-    private void setupWindowBlurListener() {
-        Consumer<Boolean> windowBlurEnabledListener = this::updateWindowForBlurs;
-        getWindow().getDecorView().addOnAttachStateChangeListener(
-                new View.OnAttachStateChangeListener() {
-                    @Override
-                    public void onViewAttachedToWindow(View v) {
-                        getWindowManager().addCrossWindowBlurEnabledListener(
-                                windowBlurEnabledListener);
-                    }
-
-                    @Override
-                    public void onViewDetachedFromWindow(View v) {
-                        getWindowManager().removeCrossWindowBlurEnabledListener(
-                                windowBlurEnabledListener);
-                    }
-                });
-    }
-
-    private void updateWindowForBlurs(boolean blursEnabled) {
-            // Set the window background blur and blur behind radii
-            getWindow().setBackgroundBlurRadius(mBackgroundBlurRadius);
     }
 
 }
